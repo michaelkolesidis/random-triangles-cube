6bc0f5b422c208339ce3cd79ca45c22f268b016f
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-05-20 14:43:43 +0300

    webpack config  update

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/script.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename:"images/[name][ext][query]",
-        // publicPath: "/",
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/index.html"})],
-    module: {
-        rules: [
-            {test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"]}, 
-            {test: /\.(png|svg|jpg|jpeg|gif|webp)$/i, type: "asset/resource"},
-        ],
-    },
+  entry: "./src/script.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext][query]",
+    // publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      { test: /\.(png|svg|jpg|jpeg|gif|webp)$/i, type: "asset/resource" },
+    ],
+  },
 };
